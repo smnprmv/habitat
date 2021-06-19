0678b775dbd73b1c7f3e980790c8241773f32cda
smnprmv <mrsam1804@gmail.com>
2021-06-19 13:26:11 +0300

    calc improved

--- src/app/components/calc/calc.component.ts
+++ src/app/components/calc/calc.component.ts
@@ -7,19 +7,81 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calc.component.scss']
 })
 export class CalcComponent implements OnInit {
-  private _ans: string = "0"
-  private _memory: number | undefined
-  private changer!: number
-  private change: boolean = true
-  private oper: boolean = false
-  public operation: 'multi' | 'divide' | 'sum' | 'substr' | undefined = undefined
-  private _history: string = ''
+  // private _ans: string = "0"
+  // private _memory: number | undefined
+  // private changer!: number
+  // private change: boolean = true
+  // private oper: boolean = false
+  // public operation: 'multi' | 'divide' | 'sum' | 'substr' | undefined = undefined
+  // private _history: string = ''
 
-  private sign = {'multi': 'x', 'divide': '/' , 'sum': '+' , 'substr': '-'}
+  // private sign = {'multi': 'x', 'divide': '/' , 'sum': '+' , 'substr': '-'}
 
   constructor() { }
+  private sceleton: (number)[] = []
+  private _ans: string = '0'
+  public _operation: '×' | '÷' | '-' | '+' | undefined =  undefined
+  private display: boolean = false
+  private _history: string = ''
 
   ngOnInit(): void {
+  }
+
+  clearMem() {
+    this.sceleton = []
+    this._history = ''
+    this._ans = '0'
+  }
+
+  changeSign () : void {
+    if (this._ans[0] == '-') {
+      this._ans = this._ans.substring(1)
+    }
+    else {
+      this._ans = '-' + this._ans
+    }
+  }
+
+  addNumber (value: string) :void {
+    if (this.display) {
+      this._ans = '0'
+      this.display = false
+    }
+
+    if (this._ans !== '0') {
+      this._ans += value
+    }
+    else this._ans = value
+  }
+
+  operation(type: '×' | '÷' | '-' | '+' | undefined) {
+    this._operation = type
+    this.equal()
+  }
+
+  equal() {
+    if (this.sceleton.length == 0 && this._ans == '0') {
+      return
+    }
+
+    if (!this.display) {
+      this.sceleton[1] = parseFloat(this._ans)
+      this.display = true
+    }
+  
+    switch (this._operation) {
+      case '×': this._ans = this.sceleton.reduce((x, y) => x * y).toString()
+        break 
+      case '÷': this._ans = this.sceleton.reduce((x, y) => x / y).toString()
+        break 
+      case '+': this._ans = this.sceleton.reduce((x, y) => x + y).toString()
+        break
+      case '-': this._ans = this.sceleton.reduce((x, y) => x - y).toString()
+        break 
+      default: return
+    }
+    this.sceleton[0] = parseFloat(this._ans)
+    this._history += this.sceleton[1] + this._operation
   }
 
   get ans (): string {
@@ -30,89 +92,80 @@ export class CalcComponent implements OnInit {
     return this._history
   }
 
-  set ans (value: string) {
-    this._ans = value
-  }
+  // set ans (value: string) {
+  //   this._ans = value
+  // }
 
-  clearMem() {
-    this._memory = undefined
-    this._history = ''
-  }
+  // addNumber (value: string) :void {
+  //   this._history += value
 
-  changeSign () : void {
-    this._ans = '-' + this._ans
-  }
+  //   this.change = true
+  //   if (this.oper) {
+  //     this._ans = '0'
+  //     this.oper = false
+  //   }
 
-  addNumber (value: string) :void {
-    this._history += value
+  //   if (this._ans == '0') {
+  //     this._ans = value
+  //   }
+  //   else {
+  //     this._ans = this._ans + value
+  //   }
+  // }
 
-    this.change = true
-    if (this.oper) {
-      this._ans = '0'
-      this.oper = false
-    }
+  // chng (type: 'multi' | 'divide' | 'sum' | 'substr' | undefined) {
+  //   this.operation = type
+  //   if (type != undefined)
+  //     this._history += this.sign[type]
 
-    if (this._ans == '0') {
-      this._ans = value
-    }
-    else {
-      this._ans = this._ans + value
-    }
-  }
+  //   this.oper = true
+  //   if (this._ans == '0') {
+  //     return
+  //   }
+  //   if (this.change) {
+  //     this.changer = parseFloat(this._ans)
+  //     this.change = false
+  //   }
 
-  chng (type: 'multi' | 'divide' | 'sum' | 'substr' | undefined) {
-    this.operation = type
-    if (type != undefined)
-      this._history += this.sign[type]
+  //   if (this._memory == undefined) {
+  //     this._memory = parseFloat(this._ans)
+  //   }
+  //   else {
+  //     switch (this.operation) {
+  //       case 'multi': {
+  //         this._ans = (this._memory * this.changer).toString()
+  //       this._memory = parseFloat(this._ans)
+  //       }
+  //       break
 
-    this.oper = true
-    if (this._ans == '0') {
-      return
-    }
-    if (this.change) {
-      this.changer = parseFloat(this._ans)
-      this.change = false
-    }
+  //       case 'divide': {
+  //         this._ans = (this._memory / this.changer).toString()
+  //       this._memory = parseFloat(this._ans)
+  //       }
+  //       break
 
-    if (this._memory == undefined) {
-      this._memory = parseFloat(this._ans)
-    }
-    else {
-      switch (this.operation) {
-        case 'multi': {
-          this._ans = (this._memory * this.changer).toString()
-        this._memory = parseFloat(this._ans)
-        }
-        break
+  //       case 'sum': {
+  //         this._ans = (this._memory + this.changer).toString()
+  //       this._memory = parseFloat(this._ans)
+  //       }
+  //       break
 
-        case 'divide': {
-          this._ans = (this._memory / this.changer).toString()
-        this._memory = parseFloat(this._ans)
-        }
-        break
-
-        case 'sum': {
-          this._ans = (this._memory + this.changer).toString()
-        this._memory = parseFloat(this._ans)
-        }
-        break
-
-        case 'substr': {
-          this._ans = (this._memory - this.changer).toString()
-        this._memory = parseFloat(this._ans)
-        }
-        break
-      }
+  //       case 'substr': {
+  //         this._ans = (this._memory - this.changer).toString()
+  //       this._memory = parseFloat(this._ans)
+  //       }
+  //       break
+  //     }
       
-    }
+  //   }
 
-  }
+  // }
 
-  equal () {
-    if (this.operation == undefined) {
-      return
-    }
-    this.chng(this.operation)
-    this._history = ''
-  }
+  // equal () {
+  //   if (this.operation == undefined) {
+  //     return
+  //   }
+  //   this.chng(this.operation)
+  //   this._history = ''
+  // }
 }
